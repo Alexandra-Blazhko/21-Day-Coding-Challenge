774dddaa148e8cf8d6c8a42f2d70cfc1c23ab7f7
zzzfairy <zzzfairy@mail.ru>
2019-05-21 17:12:47 +0300

    Solve 20 and 21 last challenges

--- 11 days done.js
+++ 11 days done.js
@@ -137,3 +137,47 @@ function configureBroadcast() {
   sendBroadcast();
 }
 configureBroadcast()
+
+function decodeMessage(message) {
+  let arr = message.split('');
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] == 1) {
+      arr[i] = "i"
+    }
+    else if (arr[i] == 3) {
+      arr[i] = "e"
+    }
+    else if (arr[i] == "0") {
+      arr[i] = "o"
+    }
+    else if (arr[i] == 5) {
+      arr[i] = "y"
+    }
+    else if (arr[i] == 4) {
+      arr[i] = "a"
+    }
+    if (arr[i] == 2) {
+      arr[i] = "u"
+    }
+    arr[73] = 'l';
+  }
+  message = arr.join('');
+  return message;
+}
+//console.log(decodeMessage('th1s 1s 4 t3st. th1s 1s 0nl5 4 t3st. 1f th1s w3r3 4 r34l m3ss4g3, 502 w021d g3t s0m3th1ng m34n1ngf2l.'))
+
+function returnToEarth() {
+    let a = broadcast("x");  
+    let b = broadcast("y");
+    let c = broadcast("z");
+    
+    let x = parseInt(decodeMessage(a), 16);
+    let y = parseInt(decodeMessage(b), 16);
+    let z = parseInt(decodeMessage(c), 16)
+    
+    navigation.x = x;
+    navigation.y = y;
+    navigation.z = z;
+    }
+    returnToEarth()
+    
